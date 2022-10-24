2b9a7a4bc489bd8eab42f4f1037d48bb60d90533
Yusuke Goto <sample@email.com>
2022-10-24 10:16:01 +0900

    最小更新、最大更新、繰り返し二乗報をライブラリ化。デバッグマクロはループ内でコールしたとき非常に遅いため削除

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
 #![allow(unused_macros, unused_imports)]
 use proconio::input;
 use proconio::marker::Usize1;
+use std::ops::Div;
 
-macro_rules! dbg {
-    ($($xs:expr),+) => {
-        if cfg!(debug_assertions) {
-            std::dbg!($($xs),+)
-        } else {
-            ($($xs),+)
+fn main() {
+}
+
+/*************************************************************************************/
+/*************************************************************************************/
+
+pub trait ChangeMinMax {
+    fn chmin(&mut self, rhs: Self) -> bool;
+    fn chmax(&mut self, rhs: Self) -> bool;
+}
+impl<T: PartialOrd + Copy> ChangeMinMax for T {
+    fn chmin(&mut self, rhs: Self) -> bool {
+        let mut ret = false;
+        if *self > rhs {
+            *self = rhs;
+            ret = true;
         }
+        ret
+    }
+    fn chmax(&mut self, rhs: Self) -> bool {
+        let mut ret = false;
+        if *self < rhs {
+            *self = rhs;
+            ret = true;
+        }
+        ret
     }
 }
 
-fn main() {
+pub trait RepeatedSquaring {
+    fn power(self, p: i32) -> Self;
+}
+impl<T: std::ops::MulAssign + std::ops::Div<Output = T> + Copy> RepeatedSquaring for T {
+    fn power(self, mut p: i32) -> Self {
+        #[allow(clippy::eq_op)]
+        let mut ret: Self = self / self;
+        let mut mul: Self = self;
+        while p > 0 {
+            if p & 1 != 0 {
+                ret *= mul;
+            }
+            p >>= 1;
+            mul *= mul;
+        }
+        ret
+    }
 }
