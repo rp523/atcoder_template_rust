ccf1bfe2365dbbc2fa5beef6611cd9338fe97ceb
Yusuke Goto <sample@email.com>
2022-10-28 01:10:22 +0900

    sccの一部関数をprivate化

--- src/main.rs
+++ src/main.rs
@@ -961,85 +961,88 @@ impl Line2d {
     }
 }
 
-struct Scc {
-    n: usize,
-    pub graph: Vec<Vec<usize>>,
-    bwd_graph: Vec<Vec<usize>>,
-}
-impl Scc {
-    pub fn new(n: usize) -> Scc {
-        Scc {
-            n,
-            graph: vec![vec![]; n],
-            bwd_graph: vec![vec![]; n],
-        }
+mod strongly_connected_component {
+    pub struct StronglyConnectedComponent {
+        n: usize,
+        pub graph: Vec<Vec<usize>>,
+        bwd_graph: Vec<Vec<usize>>,
     }
-    fn add(&mut self, from: usize, into: usize) {
-        self.graph[from].push(into);
-        self.bwd_graph[into].push(from);
-    }
-    pub fn decompose(&mut self) -> Vec<Vec<usize>> {
-        let mut scc = Vec::<Vec<usize>>::new();
-        let mut fwd_seen = vec![false; self.n];
-        let mut order = Vec::<usize>::new();
-        for i in 0..self.n {
-            if !fwd_seen[i] {
-                Scc::fwd_dfs(&self.graph, i, None, &mut fwd_seen, &mut order);
+    impl StronglyConnectedComponent {
+        pub fn new(n: usize) -> StronglyConnectedComponent {
+            StronglyConnectedComponent {
+                n,
+                graph: vec![vec![]; n],
+                bwd_graph: vec![vec![]; n],
             }
         }
-        order.reverse();
-        let mut bwd_seen = vec![false; self.n];
-        for i_ in &order {
-            let i = *i_;
-            if !bwd_seen[i] {
-                let mut grp = Vec::<usize>::new();
-                Scc::bwd_dfs(&self.bwd_graph, i, None, &mut bwd_seen, &mut grp);
-                grp.reverse();
-                scc.push(grp);
-            }
+        pub fn add(&mut self, from: usize, into: usize) {
+            self.graph[from].push(into);
+            self.bwd_graph[into].push(from);
         }
-        scc
-    }
-    fn bwd_dfs(
-        graph: &Vec<Vec<usize>>,
-        v: usize,
-        pre: Option<usize>,
-        seen: &mut Vec<bool>,
-        grp: &mut Vec<usize>,
-    ) {
-        seen[v] = true;
-        for nv_ in &graph[v] {
-            let nv = *nv_;
-            if let Some(pre_v) = pre {
-                if nv == pre_v {
-                    continue;
+        pub fn decompose(&mut self) -> Vec<Vec<usize>> {
+            let mut scc = Vec::<Vec<usize>>::new();
+            let mut fwd_seen = vec![false; self.n];
+            let mut order = Vec::<usize>::new();
+            for i in 0..self.n {
+                if !fwd_seen[i] {
+                    StronglyConnectedComponent::fwd_dfs(&self.graph, i, None, &mut fwd_seen, &mut order);
                 }
             }
-            if !seen[nv] {
-                Scc::bwd_dfs(graph, nv, Some(v), seen, grp);
-            }
-        }
-        grp.push(v);
-    }
-    fn fwd_dfs(
-        graph: &Vec<Vec<usize>>,
-        v: usize,
-        pre: Option<usize>,
-        seen: &mut Vec<bool>,
-        order: &mut Vec<usize>,
-    ) {
-        seen[v] = true;
-        for nv_ in &graph[v] {
-            let nv = *nv_;
-            if let Some(pre_v) = pre {
-                if nv == pre_v {
-                    continue;
+            order.reverse();
+            let mut bwd_seen = vec![false; self.n];
+            for i_ in &order {
+                let i = *i_;
+                if !bwd_seen[i] {
+                    let mut grp = Vec::<usize>::new();
+                    StronglyConnectedComponent::bwd_dfs(&self.bwd_graph, i, None, &mut bwd_seen, &mut grp);
+                    grp.reverse();
+                    scc.push(grp);
                 }
             }
-            if !seen[nv] {
-                Scc::fwd_dfs(graph, nv, Some(v), seen, order);
-            }
+            scc
         }
-        order.push(v);
+        fn bwd_dfs(
+            graph: &Vec<Vec<usize>>,
+            v: usize,
+            pre: Option<usize>,
+            seen: &mut Vec<bool>,
+            grp: &mut Vec<usize>,
+        ) {
+            seen[v] = true;
+            for nv_ in &graph[v] {
+                let nv = *nv_;
+                if let Some(pre_v) = pre {
+                    if nv == pre_v {
+                        continue;
+                    }
+                }
+                if !seen[nv] {
+                    StronglyConnectedComponent::bwd_dfs(graph, nv, Some(v), seen, grp);
+                }
+            }
+            grp.push(v);
+        }
+        fn fwd_dfs(
+            graph: &Vec<Vec<usize>>,
+            v: usize,
+            pre: Option<usize>,
+            seen: &mut Vec<bool>,
+            order: &mut Vec<usize>,
+        ) {
+            seen[v] = true;
+            for nv_ in &graph[v] {
+                let nv = *nv_;
+                if let Some(pre_v) = pre {
+                    if nv == pre_v {
+                        continue;
+                    }
+                }
+                if !seen[nv] {
+                    StronglyConnectedComponent::fwd_dfs(graph, nv, Some(v), seen, order);
+                }
+            }
+            order.push(v);
+        }
     }
 }
+use strongly_connected_component::StronglyConnectedComponent as Scc;
