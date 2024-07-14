f2433980a5ac49555e200128d2c2ead220b8aaff
Yusuke Goto <sample@email.com>
2024-07-14 18:15:19 +0900

    implement longest-common-prefix

--- src/main.rs
+++ src/main.rs
@@ -2723,7 +2723,7 @@ mod suffix_array {
         let rj1 = if j + k <= n { pos_to_ord[j + k] } else { -1 };
         (ri0, ri1).cmp(&(rj0, rj1))
     }
-    fn construct_sa(s: &[usize]) -> Vec<usize> {
+    fn construct_suffix_array(s: &[usize]) -> Vec<usize> {
         let n = s.len();
         let mut ord_to_pos = vec![0usize; n + 1];
         let mut pos_to_ord = vec![0i64; n + 1];
@@ -2736,9 +2736,9 @@ mod suffix_array {
         while k <= n {
             ord_to_pos.sort_by(|&i, &j| compare(&pos_to_ord, i, j, k, n));
             pos_to_ord_nxt[ord_to_pos[0]] = 0;
-            for i in 1..=n {
-                pos_to_ord_nxt[ord_to_pos[i]] = pos_to_ord_nxt[ord_to_pos[i - 1]]
-                    + if compare(&pos_to_ord, ord_to_pos[i - 1], ord_to_pos[i], k, n)
+            for ord in 1..=n {
+                pos_to_ord_nxt[ord_to_pos[ord]] = pos_to_ord_nxt[ord_to_pos[ord - 1]]
+                    + if compare(&pos_to_ord, ord_to_pos[ord - 1], ord_to_pos[ord], k, n)
                         == Ordering::Less
                     {
                         1
@@ -2752,12 +2752,43 @@ mod suffix_array {
         }
         ord_to_pos
     }
+    fn construct_longest_common_prefix(s: &[usize], ord_to_pos: &[usize]) -> Vec<usize> {
+        let n = s.len();
+        debug_assert_eq!(ord_to_pos.len(), n + 1);
+        let pos_to_ord = {
+            let mut pos_to_ord = vec![0; ord_to_pos.len()];
+            for (ord, &pos) in ord_to_pos.iter().enumerate() {
+                pos_to_ord[pos] = ord;
+            }
+            pos_to_ord
+        };
+        let mut lcp_now = 0;
+        let mut lcp = vec![0; n];
+        for pos in 0..n {
+            let pre_ord = pos_to_ord[pos] - 1;
+            let pre_ord_pos = ord_to_pos[pre_ord];
+            if lcp_now > 0 {
+                lcp_now -= 1;
+            }
+            while pre_ord_pos + lcp_now < n && pos + lcp_now < n {
+                if s[pre_ord_pos + lcp_now] == s[pos + lcp_now] {
+                    lcp_now += 1;
+                } else {
+                    break;
+                }
+            }
+            lcp[pre_ord] = lcp_now;
+        }
+        lcp
+    }
     pub trait ToSuffixArray {
-        fn to_suffix_array(&self) -> Vec<usize>;
+        fn suffix_array(&self) -> (Vec<usize>, Vec<usize>);
     }
     impl ToSuffixArray for Vec<usize> {
-        fn to_suffix_array(&self) -> Vec<usize> {
-            construct_sa(self)
+        fn suffix_array(&self) -> (Vec<usize>, Vec<usize>) {
+            let ord_to_pos = construct_suffix_array(self);
+            let lcp = construct_longest_common_prefix(self, &ord_to_pos);
+            (ord_to_pos, lcp)
         }
     }
     mod test {
@@ -2772,10 +2803,25 @@ mod suffix_array {
             for n in 1..=N {
                 for _ in 0..T {
                     let a = (0..n).map(|_| rand.next_usize() % C).collect::<Vec<_>>();
-                    let mut expected = (0..=n).collect::<Vec<_>>();
-                    expected.sort_by(|&i, &j| a[i..].cmp(&a[j..]));
-                    let actual = a.to_suffix_array();
-                    assert_eq!(expected, actual);
+                    let sa_expected = {
+                        let mut sa_expected = (0..=n).collect::<Vec<_>>();
+                        sa_expected.sort_by(|&i, &j| a[i..].cmp(&a[j..]));
+                        sa_expected
+                    };
+                    let lcp_expected = (0..n)
+                        .map(|i| {
+                            (0..)
+                                .take_while(|&d| {
+                                    (sa_expected[i] + d < n)
+                                        && (sa_expected[i + 1] + d < n)
+                                        && a[sa_expected[i] + d] == a[sa_expected[i + 1] + d]
+                                })
+                                .count()
+                        })
+                        .collect::<Vec<_>>();
+                    let (sa_actual, lcp_actual) = a.suffix_array();
+                    assert_eq!(sa_expected, sa_actual);
+                    assert_eq!(lcp_expected, lcp_actual);
                 }
             }
         }
